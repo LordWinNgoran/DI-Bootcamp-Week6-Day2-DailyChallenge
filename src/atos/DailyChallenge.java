6aa914d3e293e62417a992f84ddea76171718ae8
LordWinNgoran <cyrille4011@gmail.com>
2023-02-05 15:46:23 +0000

    update

--- src/atos/DailyChallenge.java
+++ src/atos/DailyChallenge.java
@@ -1,26 +1,32 @@
+//Daily Challenge: Coin Flip
+/*
+ * @author: N'goran Kouadio Jean Cyrille
+ * @description: Write a Java program that uses the loops to produce the following output:
+ * Date : 01/01/2023
+ */
+
 package atos;
 import java.util.Random;
 public class DailyChallenge {
 	public static void main(String[] args) {
-        // Création d'un objet pour générer des nombres aléatoires
-        
+		
+        // create a new random number generator object
         Random rand = new Random();
         
-        // Choisir un nombre aléatoire entre 0 et 1
-        
+     // pick a random number between 0 and 1:
         int n = rand.nextInt(2);
         
-        // Afficher le nombre choisi
+        // show the number
         
-        System.out.println("Valeur aléatoire choisie: " + n);
+        System.out.println("Number picked is : " + n);
 
-        // Si n est égal à 0, afficher "Pile"
+        // if n = 0 show Heads
         if (n == 0) {
-            System.out.println("Pile");
+            System.out.println("Heads");
         }
-            // Sinon, afficher "Tail"
+            // else show "Tail"
         else {
-            System.out.println("Tail");
+            System.out.println("Tails");
         }
     }
 }
